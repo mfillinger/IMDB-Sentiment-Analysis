186b5a421fe9904aa35bfde3ecbf432f91c58c73
mfillinger <107496293+mfillinger@users.noreply.github.com>
2026-04-01 12:35:57 -0400

    Added initial data preprocessing and TF-IDF vectorization

---
+++ train_sentiment_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#load dataset
+df = pd.read_csv("IMDB Dataset.csv")
+
+#convert labels to numbers
+df['sentiment']= df['sentiment'].map({'positive': 1, 'negative': 0})
+
+#split data
+X_train, X_test, y_train, y_test = train_test_split(
+    df['review'], df['sentiment'], test_size=0.2, random_state=42
+)
+
+#tf-idf vectorization
+vectorizer= TfidfVectorizer(max_features=5000)
+
+X_train_tfidf= vectorizer.fit_transform(X_train)
+X_test_tfidf= vectorizer.transform(X_test)
+
+print("Data loaded and vectorized successfully")
+print("Training samples:", X_train_tfidf.shape)
+print("Testing samples:", X_test_tfidf.shape)
