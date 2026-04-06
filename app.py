666e73300700441854ad44d574b4f570f1dcbeb3
mfillinger <107496293+mfillinger@users.noreply.github.com>
2026-04-06 15:56:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import streamlit as st
 import joblib
 
 #load saved model and vectorizer
-model= joblib.load("sentiment_model.pkl")
+model= joblib.load("best_sentiment_model.pkl")
 vectorizer= joblib.load("tfidf_vectorizer.pkl")
 
 st.title("IMDB Sentiment Analysis")
 st.write("Enter a movie review and the model will predict the sentiment.")
 
-#user input
 review= st.text_area("Movie Review")
 
 if st.button("Predict"):
@@ -16,7 +15,7 @@ if st.button("Predict"):
         st.warning("Please enter a review.")
     else:
         review_tfidf = vectorizer.transform([review])
-        prediction = model.predict(review_tfidf)
+        prediction= model.predict(review_tfidf)
 
         if prediction[0] == 1:
             st.success("Positive Sentiment")
