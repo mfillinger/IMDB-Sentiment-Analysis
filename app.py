d7469a4b727616d45d6a499c92ebddaeed995a61
mfillinger <107496293+mfillinger@users.noreply.github.com>
2026-04-07 17:25:51 -0400

    Enhance UI with model details in sentiment analysis
    Added model and dataset information to the UI.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ model= joblib.load("best_sentiment_model.pkl")
 vectorizer= joblib.load("tfidf_vectorizer.pkl")
 
 st.title("IMDB Sentiment Analysis")
+st.caption("Model: TF-IDF + Logistic Regression | Dataset: IMDB 50K Reviews")
 st.write("Enter a movie review and the model will predict the sentiment.")
 
 review= st.text_area("Movie Review")
